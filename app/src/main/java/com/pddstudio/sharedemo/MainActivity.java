7b6a119817a4b608c2e59acdd8c6d57a0a3e8e15
Patrick J <patrick.pddstudio@googlemail.com>
2016-02-27 14:45:12 +0100

    Added some basic usage to sample app

--- app/src/main/java/com/pddstudio/sharedemo/MainActivity.java
+++ app/src/main/java/com/pddstudio/sharedemo/MainActivity.java
@@ -20,12 +20,54 @@ package com.pddstudio.sharedemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import com.pddstudio.share.Share;
+import com.pddstudio.share.enums.Type;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText chooserTitleText;
+    EditText shareContentText;
+    Button shareButton;
+    CheckBox useIntentChooser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        chooserTitleText = (EditText) findViewById(R.id.intentChooserText);
+        shareContentText = (EditText) findViewById(R.id.sharedContentText);
+
+        shareButton = (Button) findViewById(R.id.shareButton);
+        shareButton.setOnClickListener(this);
+
+        useIntentChooser = (CheckBox) findViewById(R.id.useChooserCheckBox);
+
     }
+
+    @Override
+    public void onClick(View v) {
+        String chooserTitle;
+        String shareText;
+
+        if(chooserTitleText.getText().toString().isEmpty()) chooserTitle = getString(R.string.share_chooser_title);
+        else chooserTitle = chooserTitleText.getText().toString();
+        if(shareContentText.getText().toString().isEmpty()) shareText = getString(R.string.share_default_text);
+        else shareText = shareContentText.getText().toString();
+
+        //share content via 'Share'
+
+        Share.create(this)
+                .withCustomChooserTitle(chooserTitle)
+                .withText(shareText)
+                .withType(Type.PLAIN_TEXT)
+                .share(useIntentChooser.isChecked());
+
+    }
+
 }
